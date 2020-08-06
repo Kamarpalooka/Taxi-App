765c46d06b4ebd4d0c2831784888e552cf4a4de2
Kamar Palooka <Kamarpalooka@mail.com>
2020-08-06 09:30:03 -0400

    Trip module added

--- taxi/taxi/urls.py
+++ taxi/taxi/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView  # new
 
 from trips.views import SignUpView, LogInView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/v1/auth/', include('rest_framework.urls')),
+
     path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
-    path('api/log_in/', LogInView.as_view(), name='log_in'),  # new
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # new
+    path('api/log_in/', LogInView.as_view(), name='log_in'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/trip/', include('trips.urls', 'trip',)),
 ]
