23db7b1617c18adcd8cf4248ed273c62dfbb46c1
Kamar Palooka <Kamarpalooka@mail.com>
2020-08-07 07:13:32 -0400

    Add Rider to all trip-related groups he belongs to when the socket connects and remove Rider when socket disconnect

--- taxi/trips/consumers.py
+++ taxi/trips/consumers.py
@@ -6,6 +6,7 @@ Connect to channels router
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 
+from trips.models import Trip
 from trips.serializers import NestedTripSerializer, TripSerializer
 
 
@@ -13,6 +14,18 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
     @database_sync_to_async
     def _get_user_group(self, user):
         return user.groups.first().name
+
+    @database_sync_to_async
+    def _get_trip_ids(self, user):
+        """
+        Get Trip-IDs of user(Driver/Rider)
+        """
+        user_groups = user.groups.values_list('name', flat=True)
+        if 'driver' in user_groups:
+            trip_ids = user.trips_as_driver.exclude(status=Trip.COMPLETED).only('id').values_list('id', flat=True)
+        else:
+            trip_ids = user.trips_as_rider.exclude(status=Trip.COMPLETED).only('id').values_list('id', flat=True)
+        return map(str, trip_ids)
 
     async def connect(self):
         user = self.scope['user']
@@ -23,6 +36,15 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
             if user_group == 'driver':
                 await self.channel_layer.group_add(
                     group='drivers',
+                    channel=self.channel_name
+                )
+
+            """
+            Add the rider to all of trip-related groups he belongs to when the WebSocket connects.
+            """
+            for trip_id in await self._get_trip_ids(user):
+                await self.channel_layer.group_add(
+                    group=trip_id,
                     channel=self.channel_name
                 )
             await self.accept()
@@ -75,6 +97,15 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         if user_group == 'driver':
             await self.channel_layer.group_discard(
                 group='drivers',
+                channel=self.channel_name
+            )
+
+        """
+        Remove rider from all of trip-related groups he belongs to when the WebSocket disconnects.
+        """
+        for trip_id in await self._get_trip_ids(user):
+            await self.channel_layer.group_discard(
+                group=trip_id,
                 channel=self.channel_name
             )
         await super().disconnect(code)
