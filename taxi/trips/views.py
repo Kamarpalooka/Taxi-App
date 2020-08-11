465fc39ed47e9b2cc17123b3aeb37e3b7e11e5ab
Kamar Palooka <Kamarpalooka@mail.com>
2020-08-11 07:11:06 -0400

    TripView queryset updated to proper filter user trip per it related group

--- taxi/trips/views.py
+++ taxi/trips/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -11,7 +12,7 @@ class SignUpView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class LogInView(TokenObtainPairView): # new
+class LogInView(TokenObtainPairView):
     serializer_class = LogInSerializer
 
 
@@ -19,5 +20,14 @@ class TripView(viewsets.ReadOnlyModelViewSet):
     lookup_field = 'id'
     lookup_url_kwarg = 'trip_id'
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.group == 'driver':
+            return Trip.objects.filter(
+                Q(status=Trip.REQUESTED) | Q(driver=user)
+            )
+        if user.group == 'rider':
+            return Trip.objects.filter(rider=user)
+        return Trip.objects.none()
