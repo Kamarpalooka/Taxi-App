fe8a237b1bb1af72929ba4ce2862e19912f11e5b
Kamar Palooka <Kamarpalooka@mail.com>
2020-08-11 07:32:42 -0400

    TripView serializer updated to include driver and rider objects representation

--- taxi/trips/serializers.py
+++ taxi/trips/serializers.py
@@ -31,7 +31,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'password1', 'password2', 'first_name', 'last_name', 'group',)
+        fields = ('id', 'username', 'password1', 'password2', 'first_name', 'last_name', 'group', 'photo',)
         read_only_fields = ('id',)
 
 
